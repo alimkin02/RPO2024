f2c689400f8403329b6fd4d9031ff03bead44128
alimkin02 <alimkin02@yandex.ru>
2024-03-29 13:09:20 +0300

    4th lab done (with backend)

---
+++ backend/src/main/java/ru/iu3/backend/BackEndSampleApplication.java
@@ -0,0 +1,13 @@
+package ru.iu3.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndSampleApplication.class, args);
+	}
+
+}
